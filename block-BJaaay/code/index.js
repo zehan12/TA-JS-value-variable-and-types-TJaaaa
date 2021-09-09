791218de9f12eff2c4febff4dda6a47edaec27b0
Zehan Khan <zehan9211@gmail.com>
2021-09-09 17:21:05 +0530

    using loops

--- block-BJaaay/code/index.js
+++ block-BJaaay/code/index.js
@@ -11,3 +11,47 @@
 // Calculate the sum of all numbers from 1 to 10.
 
 // Log all the values from 1 to 10 using while loop
+
+
+// log all the values from 1 to 10
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+console.log(6);
+console.log(7);
+console.log(8);
+console.log(9);
+console.log(10);
+
+
+// log all the even value from 1 to 10.
+
+for (let i = 1 ; i < 10 ; i = i + 1){
+    if ( i %2 == 0 ){
+        console.log(i)
+    }
+}
+
+// log all the odd value from 1 to 10.
+
+for (let i = 1 ; i < 10 ; i = i + 1){
+    if ( i %2 !== 0 ){
+        console.log(i)
+    }
+}
+
+// calculate all the number 1 to 10
+let sum = 0;
+for (let i = 1; i <= 10 ; i = i +1 ){
+    sum = sum + i
+}
+console.log(sum);
+
+// log all value 1 to 10
+
+for (let i = 1; i = 10 ; i = i + 1){
+    console.log(i)
+}
+
