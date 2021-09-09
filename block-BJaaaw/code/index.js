002230e44b78e319a8b4298de7632868b28485a6
Zehan Khan <zehan9211@gmail.com>
2021-09-09 15:48:18 +0530

    making decision in code

--- block-BJaaaw/code/index.js
+++ block-BJaaaw/code/index.js
@@ -55,3 +55,149 @@ Implement the condition give below using`if..else` and`switch` statement.
   - If`freezing` alert`Get your sweeter on`
   - Anything else should alert`Not a valid input`
 */
+
+
+//1 ask user for the number even or odd
+let num = prompt(`Enter any number`);
+
+if (num % 2 == 0){
+  alert(`The Number you enter is Even : ${num}`)
+} else if (num % 2 !==0){
+  alert(`The Number you enter is Odd : ${num}`)
+}
+
+
+//4 write a program accept value from a user
+
+let numA = prompt('Enter number A');
+let numB = prompt(`Enter number B`);
+
+if (numA > numB) {
+  alert(`A is greater than B`);
+} else if (numA < numB) {
+  alert(`B is greater than A`)
+}
+
+
+//3 ask user for the number even or odd in terniary 
+(num % 2 == 0) ? alert(`The Number you enter is Even : ${num}`) : alert(`The Number you enter is Odd : ${num}`)
+
+
+// 4 ask user to enter house name 
+let house = prompt(`Enter any house name`);
+
+
+if (house == 'stark' ){
+  alert(" Winter is coming");
+} else if (house == 'lannister' ) {
+  alert(" A lannister always pays his debt");
+} else {
+  alert("All men must die");
+}
+
+
+//5 ask user to enter house name by ternary
+let house = prompt(`Enter any house name`);
+
+house == "stark" ? alert("Winter is comming"): alert("All men must die");
+
+
+//6 take month no.
+
+let month = prompt("Enter any month no.");
+
+switch (month){
+   case '1':
+   case '3':
+   case '5':
+   case '7':
+   case '8':
+   case '10':
+   case '12':  
+      alert("31 days");
+      break;
+   case '2':
+      alert(`29 days`);
+      break;
+   case '4':
+   case '6':
+   case '9':
+   case '11':
+     alert("30 days");
+     break;
+   default :
+     alert("Invalid")
+}
+
+
+//7 salary -in hand
+
+let Salary = prompt(`Enter in -hand Amount`);
+
+if ( Salary <= 20000 ){
+  alert(`tax is 10%`);
+} else if ( Salary <= 40000 ){
+  alert(`tax is 20%`);
+} else if ( Salary > 50000 ){
+  alert(`tax is 30%`);
+}
+
+// 8 alert the grade 
+
+
+let marks = prompt(`enter your Marks`);
+
+if ( marks > 100 ){
+  alert("Marks can't be greater than 100")
+}else if ( marks > 80){
+  alert("Grade A");
+} else if ( marks > 80){
+  alert("Grade A");
+} else if ( marks > 50){
+  alert("Grade B");
+} else if ( marks > 30 ){
+  alert("Grade C");
+} else if ( marks > 0 );{
+  alert("Grade D");
+}
+
+// alert the grade with switch
+
+let marks = prompt("enter your Marks");
+
+switch(true){
+  case (marks > 80 && marks < 100): // (marks < 80) == true;
+    alert("Grade A");
+    break;
+  
+  case (marks > 50 && marks < 80) :
+     alert("Grade B");
+     break;
+
+  case (marks > 80 && marks < 50) :
+    alert("Grade C");
+    break;
+  
+  case (marks > 0) :
+    alert("Grade D");
+    break;
+}
+
+
+
+
+// 9 wheater outside 
+
+let wheater = prompt(`What is the weather like outside?`);
+
+if ( wheater == `sunny`){
+   alert(`Wear a T-shirt`);
+} else if ( wheater == `rainy`){
+  alert(`Don't forget to take your raincoat`);
+} else if ( wheater == `hot`){
+  alert(`Get a hanky`);
+} else if ( wheater == 'freezing'){
+  alert(`Get your sweeter on`)
+} else {
+  alert(`Not a valid input`);
+}
